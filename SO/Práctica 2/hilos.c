57391686bebca2624780a1cdf6e1b1dd737524b2
AlexTheMagnus <i72lulem@uco.es>
2018-10-18 18:41:16 +0200

    Añadida P2

---
+++ SO/Práctica 2/hilos.c
@@ -0,0 +1,73 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+double counter = 0;
+/*Eligiendo es un int porque no existen los bool en c.
+ El 1 será considerado true y el 0 false.*/
+int Eligiendo[NHILOS];
+int Numero[NHILOS];
+
+//Algoritmo de Lamport: inicialización
+for(int i = 0; i < NHILOS; i++){
+  Eligiendo[i] = 0;
+  Numero[i] = 0;
+}
+//-------------------------------------
+
+#define ITER	1000
+#define NHILOS	4
+
+int main()
+{
+    pthread_t hilos[NHILOS];
+    int status, i, v[NHILOS];
+    extern double counter;
+    void *adder(void *);
+    double *r_value;
+
+    // Create NHILOS threads
+    for (i = 0; i < NHILOS; i++) {
+	v[i] = i;
+	if ((status = pthread_create(&hilos[i], NULL, adder, (void *) &v[i])))
+	    exit(status);
+    }
+
+    // Wait threads
+    for (i = 0; i < NHILOS; i++) {
+	pthread_join(hilos[i], (void **) &r_value);
+	printf("Value returned by %lu thread: %lf\n", hilos[i], *r_value);
+    }
+
+    // Final result
+    fprintf(stdout, "%f\n", counter);
+
+    return 0;
+}
+
+void *adder(void *p)
+{
+    double l, *to_return;
+    extern double counter;
+    extern int Eligiendo;
+    extern int Numero;
+    int *id, i;
+
+    id = (int *) p;
+
+    for (i = 0; i < ITER; i++) {
+      //Sección Crítica
+    	l = counter;
+    	fprintf(stdout, "Hilo %d: %f\n", *id, counter);
+    	l++;
+    	counter = l;
+      //Fin Sección Crítica
+    }
+
+    to_return = malloc(sizeof(double));
+
+    *to_return = counter;
+
+    pthread_exit((void *) to_return);
+}

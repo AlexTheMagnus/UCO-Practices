ddbc27b24fd9a271dfdac4e85f0fba8be2fad65f
AlexTheMagnus <i72lulem@uco.es>
2019-02-26 18:29:07 +0100

    Añadida P1

---
+++ Practica-1-usuario/funcionesAuxiliares.hpp
@@ -0,0 +1,37 @@
+/*!
+  \file   funcionesAuxiliares.hpp
+  \brief  Funciones auxiliares para el programa principal de la práctica 1
+*/
+
+
+#ifndef _FUNCIONESAUXILIARES_HPP_
+#define _FUNCIONESAUXILIARES_HPP_
+
+namespace ed
+{
+	/*!
+		\brief Muestra el menú del programa principal
+		\return Devuelve el número de opción elegido
+	*/
+	int menu();
+
+	/*!
+		\brief Funcion de prueba de la clase monomio
+	*/
+	void testMonomio();
+
+
+	/*!
+		\brief Funcion de prueba de los operadores externos de la clase monomio
+	*/
+	void operadoresExternosMonomios();
+
+	/*!
+		\brief Funcion de prueba del método Potencia
+	*/
+	void testPotencia();
+
+} // Fin del espacio de nombre de la asignatura: ed
+
+// Fin de _FUNCIONESAUXILIARES_HPP_
+#endif
---
+++ Practica-1-usuario/html/search/all_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['getcoeficiente',['getCoeficiente',['../classed_1_1Monomio.html#a65925f0ea6ad5872120fd8419991dedf',1,'ed::Monomio']]],
+  ['getgrado',['getGrado',['../classed_1_1Monomio.html#aafd6219fa0ad099c0ce4b40f2a7c41ee',1,'ed::Monomio']]],
+  ['green',['GREEN',['../macros_8hpp.html#acfbc006ea433ad708fdee3e82996e721',1,'macros.hpp']]]
+];

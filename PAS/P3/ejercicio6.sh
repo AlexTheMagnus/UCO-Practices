0a96201385e0ae1c6492d6d30336df59bb1e3056
AlexTheMagnus <i72lulem@uco.es>
2019-04-15 13:25:44 +0200

    Añadido ejercicio6.sh

--- PAS/P3/ejercicio6.sh
+++ PAS/P3/ejercicio6.sh
@@ -2,17 +2,42 @@
 #  arg1 = carpeta
 
 function listar_archivos() {
-    echo "<ul>"
     for x in $(find $1 -maxdepth 1)
-        echo "<li><strong>$x</strong></li>"
+    do
+        if [ -d $x ] && [ $x != $1 ]; then
+            echo "<ul><li><strong>$x</strong></li>" >> $output
+            listar_archivos $x
+            echo "</ul>" >> $output
+        elif [ $x != $1 ]; then
+            echo "<li>$x</li>" >> $output
+        fi
     done
 }
 
-echo "<html>"
-echo "<head>"
-echo "<title><h3>listardo de dependencias de $1 </h3></title>"
-echo "</head>"
-echo "<body>"
-listar_archivos($1)
-echo "</body>"
-echo "</html>"
+output=$1.html
+
+if [ $# -ne 1 ]; then
+    echo "Error. El método de ejecución correcto es: ./ejercicio6 <directorio>"
+else
+    cd $1
+    echo "Generando el listado de la carpeta $(pwd)/ sobre el fichero $output..."
+    cd ..
+
+    echo "<html>" > $output
+    echo "<head>" >> $output
+    echo "<title><h3>Listado del directorio $1</h3></title>" >> $output #<h3> ??
+    echo "</head>" >> $output
+
+    echo "<body>" >> $output
+    echo '<style type="text/css">' >> $output
+    echo "  body { font-family: sans-serif;}" >> $output
+    echo "</style>" >> $output
+    echo "<h1>Listado del directorio $1</h1>" >> $output
+    echo "<ul>" >> $output
+    listar_archivos $1
+    echo "</ul>" >> $output
+    echo "</body>" >> $output
+    echo "</html>" >> $output
+
+    echo "¡Terminado!"
+fi

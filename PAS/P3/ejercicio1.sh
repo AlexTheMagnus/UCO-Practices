47caa7b0ce3fc19c54d0c12896d21534a8b23d8e
AlexTheMagnus <i72lulem@uco.es>
2019-05-25 13:42:47 +0200

    P4/ejercicio2 finished

--- PAS/P3/ejercicio1.sh
+++ PAS/P3/ejercicio1.sh
@@ -2,9 +2,9 @@
 
 cant1=$(find $1 -name "*.c" | wc -l)
 cant2=$(find $1 -name "*.h" | wc -l)
-echo "Tenemos  $cant1 ficheros con extenion .c y $cant2 ficheros con extension .h"
+echo "Tenemos  $cant1 ficheros con extension .c y $cant2 ficheros con extension .h"
 
 for x in $(find $1 -type f -name "*.c" -o -type f -name "*.h")
 do
-    echo "El fichero $x tiene $(cat $x |wc -l) lineas y $(cat $x |wc -c) caracteres"
+    echo "El fichero $x tiene $(cat $x |wc -l) lineas y $(cat $x |wc -m) caracteres"
 done | sort -nr -k 8
